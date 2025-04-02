6bf419ef553949b31f134f2fe43dc185518fc7d4
Rodrigo Santos <58148787+DiguinhoLNS@users.noreply.github.com>
2025-04-02 00:09:15 -0300

    refatora configuração do Jest para usar um único objeto de configuração e melhora a legibilidade

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-export const testEnvironment = "jsdom";
-export const transform = {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "babel-jest"
+const jestConfig = {
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.jsx?$": "babel-jest"
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+    testMatch: [
+        "**/__tests__/**/*.(test|spec).(ts|tsx)",
+        "**/?(*.)+(test|spec).(ts|tsx)"
+    ],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    transformIgnorePatterns: [
+        '/node_modules/',
+    ],
 };
-export const moduleFileExtensions = ["ts", "tsx", "js", "jsx"];
-export const testMatch = [
-    "**/__tests__/**/*.(test|spec).(ts|tsx)",
-    "**/?(*.)+(test|spec).(ts|tsx)"
-];
-export const setupFilesAfterEnv = ["<rootDir>/jest.setup.js"];
-export const transformIgnorePatterns = [
-    '/node_modules/',
-];
+
+export default jestConfig;
